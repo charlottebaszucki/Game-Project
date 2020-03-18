bba2cb3b844fbf143d1693a71647292f25ca57d5
charlottebaszucki <charlottebaszucki@gmail.com>
2020-03-18 13:47:44 -0700

    Snowman game
    I made the avatar into a snowman and the balls into snow where when the snow hits the snowman it gets bigger, you lose when the snowman covers the screen.

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -5,22 +5,29 @@ let balls = [];
 let me;
 
 
+
 function setup() {
   createCanvas(500, 400);
 
   //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
-
+  me = new Avatar(width/2, 300, 3,10);
 }
 
 function draw(){
-	background(220);
+	background("lightblue");
+  fill("gray")
+  triangle(500,400,450,300,400,400)
+  triangle(450,400,400,350,350,400)
+  fill("white")
+  triangle(455,310,450,300,445,310)
+  triangle(410,360,400,350,390,360)
+
 
   me.drawMe();
   me.moveMe();
 
   if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
+      let  b = new Ball(random(0,width), 0, -3);
       balls.push(b);
       console.log(balls); //print the balls array to the console
     }
@@ -37,32 +44,36 @@ function draw(){
 //avatar class
 class Avatar {
 
-	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+	constructor(x,y, speed,diameter){ //every avatar needs an x value, a y value, and a speed
 		    this.x = x;
     		this.y = y;
         this.speed = speed;
+        this.diameter = diameter;
 	}
 
 	drawMe(){  // draw the running person
-    		stroke("green");
-        strokeWeight(3);
-    		fill("blue");
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
+      noStroke();
+    		fill("white");
+		    ellipse(this.x,this.y,this.diameter+10,this.diameter+10);
+        ellipse(this.x,this.y+24,this.diameter+20,this.diameter+20);
+        ellipse(this.x,this.y+55,this.diameter+30,this.diameter+30);
+        fill("black")
+        ellipse(this.x,this.y+19,3,3);
+        ellipse(this.x,this.y+24,3,3);
+        ellipse(this.x,this.y+29,3,3);
+        ellipse(this.x-3,this.y-2,3,3);
+        ellipse(this.x+3,this.y-2,3,3);
+        fill("orange")
+        triangle(this.x-3,this.y+4,this.x,this.y-3,this.x+1,this.y+4)
 	}
 
 	moveMe(){
-    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
-       this.y -= this.speed;
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the up arrow, move up by speed
+       this.x -= this.speed;
     }
 
-    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
-        this.y += this.speed;
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
+        this.x += this.speed;
     }
 	}
 
@@ -85,22 +96,23 @@ class Ball {
 
 	// draw a ball on the screen at x,y
 	drawBall(){
-    	stroke(0);
-      strokeWeight(1);
-    	fill("red");
+    	noStroke();
+    	fill("white");
 		  ellipse(this.x,this.y,10,10);
 	}
 
 	//update the location of the ball, so it moves across the screen
 	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
+
+		this.y = this.y+ 1.5;
 	}
 
 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
   	bounceBall(){
     		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
+            me.diameter = me.diameter+1
+            // this.speed = -this.speed;
+
     		}
   	}
 
